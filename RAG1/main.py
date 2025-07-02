8e6f40250e6fe15a32b3755041225c164e33a834
Slimene Fellah <slmnfellah123@gmail.com>
2025-07-02 15:35:30 +0100

    add llm to rag1

--- RAG1/main.py
+++ RAG1/main.py
@@ -4,6 +4,7 @@ from index.build_index import build_faiss_index
 from query.ask import search_index
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from llm.answer import ask_ollama
 
 # Load documents
 docs = load_markdown_docs("../docs")
@@ -17,8 +18,24 @@ embeddings_np = np.array(embeddings)
 index = build_faiss_index(embeddings_np)
 
 # Ask question
-query = "Where did Slimene work in 2020?"
+# query = "Where did Slimene work in 2020?"
+query = "With what technology SmartRecruiter was built?"
 top_docs = search_index(index, model, query, docs)
 
 print("\nTop Relevant Documents:\n")
 print("\n---\n".join(top_docs))
+
+
+top_docs = search_index(index, model, query, docs)
+context = "\n".join(top_docs)
+
+# Build prompt
+prompt = f"""You are an AI assistant answering based on the following documents:
+
+{context}
+
+Question: {query}
+Answer:"""
+
+answer = ask_ollama(prompt, model="llama2")  # or "tinyllama"
+print("Answer:\n", answer)
